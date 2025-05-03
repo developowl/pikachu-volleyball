62ba4a6162742f8efab58022bd9303fae9f97c52
Shin Jihoon <tobeabestns@naver.com>
2025-05-03 18:40:59 +0900

    refactor: 승리 점수 15점 -> 7점으로 변경

--- src/resources/js/pikavolley.js
+++ src/resources/js/pikavolley.js
@@ -68,7 +68,7 @@ export class PikachuVolleyball {
     /** @type {number[]} [0] for player 1 score, [1] for player 2 score */
     this.scores = [0, 0];
     /** @type {number} winning score: if either one of the players reaches this score, game ends */
-    this.winningScore = 15;
+    this.winningScore = 7;
 
     /** @type {boolean} Is the game ended? */
     this.gameEnded = false;
